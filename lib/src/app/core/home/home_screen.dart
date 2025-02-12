f9ab9b64b6aebf989b9a9274b268f281646883aa
Amash Chaudhry <127407331+AmashChaudhry@users.noreply.github.com>
2025-02-12 20:46:20 +0500

    Initial Commit

--- lib/src/app/core/home/home_screen.dart
+++ lib/src/app/core/home/home_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Container(
             height: 60,
             width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 15),g
             decoration: BoxDecoration(
               border: Border(
                 bottom: BorderSide(
